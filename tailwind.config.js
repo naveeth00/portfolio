a87115ec74360250ed21bacb76cdb20fbacbdbcc
naveeth00 <naveethahmad35@gmail.com>
2025-07-29 14:56:08 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      borderColor :{
+        'primary' : 'rgb(85 81 227)',
+        'secondary' : '#2b2d77',
+      }
+    },
+    fontFamily : {
+      'hero-font' : 'Sriracha'
+    }
   },
   plugins: [],
 }
-
