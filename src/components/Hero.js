54ea1936bc5ab5db31746f9f182ab0fe1d31d9ea
naveeth00 <naveethahmad35@gmail.com>
2025-07-29 14:49:20 +0530

    first commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
     }
 
 
-    return <section className='flex flex-col md:flex-row px-5 py-32 bg-primary justify-center'>
+    return <section className='flex flex-col md:flex-row px-5 py-30 bg-primary justify-center'>
        <div className='md:w-1/2 flex flex-col'>
             <h1 className=' text-white text-6xl font-hero-font'>Hi, <br/> Im <span className='text-black'>JV</span> Logesh 
                 <p className='text-2xl'>{config.subtitle}</p>
